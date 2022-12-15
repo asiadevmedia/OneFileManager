374b2d75126095c0f6d9a97d5ef08807e585e498
Asiadev Media <35838486+asiadevmedia@users.noreply.github.com>
2022-12-16 05:40:07 +0700

    Add files via upload

---
+++ src/js-filemanager.php
@@ -0,0 +1,17 @@
+<?php
+namespace OneFileManager;
+
+class JsManager
+{
+    public function __construct()
+    {
+        $this->js();
+    }
+
+    public function js() {?>
+        
+        <div></div>
+        php?>
+    }
+    
+}
